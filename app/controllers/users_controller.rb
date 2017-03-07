74c302a2818340da25107df49c03e00611412d76
Traci7822 <Tracir@gmail.com>
2017-03-07 15:47:49 -0700

    create new user for form and rendering form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+    render :'layouts/form'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :users
-  root 'application#form'
+  root 'user#new'
 end
